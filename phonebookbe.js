8c26d36697498566a841089a3e98b2ab4c69893f
Thakin Aung Lin <linoscar724@gmail.com>
2022-07-07 21:53:08 +0630

    server_1

---
+++ phonebookbe.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello<h1/>");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+const countnum = persons.length;
+const date = new Date();
+app.get("/api/persons/info", (req, res) => {
+  res.send(`<h2>Phonebook has info for ${countnum} people  <h2/><br/>${date}`);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const query = Number(req.params.id);
+  const filteredperson = persons.find((el) => el.id === query);
+  console.log(filteredperson);
+  if (filteredperson) {
+    res.json(filteredperson);
+  } else res.status(404).end("404");
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const query = Number(req.params.id);
+  persons = persons.filter((el) => el.id !== query);
+  //   console.log(persons);
+  res.status(202).end();
+});
+
+const ID = Math.trunc(Math.random() * 100 + 1);
+console.log(ID);
+app.post("/api/persons", (req, res) => {
+  const newNameandPhone = req.body;
+  const number = newNameandPhone.number;
+
+  if (!newNameandPhone.name || !number) {
+    return res.status(404).json({ err: "The name or  number   is missing" });
+  }
+
+  const newObj = {
+    id: ID,
+    name: newNameandPhone.name,
+    number: newNameandPhone.number,
+  };
+
+  persons = persons.concat(newObj);
+  console.log(persons);
+  res.json(newObj);
+});
+
+const port = 3001;
+app.listen(port, () => console.log("Server is running"));
---
+++ server.js
@@ -0,0 +1,91 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2022-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+
+const requestlogger = (request, response, next) => {
+  console.log("Method :", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log(".......");
+  console.log();
+  next();
+};
+
+app.use(express.json());
+app.use(requestlogger);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello<h1/>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const ID = Number(req.params.id);
+
+  const note = notes.find((el) => el.id === ID);
+  console.log(note);
+
+  if (note) res.json(note);
+  else res.status(404).end();
+});
+
+const generateID = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((el) => el.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+
+  if (!note.content) {
+    return res.status(404).json({
+      error: "Content is missing",
+    });
+  }
+
+  const newnote = {
+    content: note.content,
+    id: generateID(),
+    important: note.important || false,
+    date: new Date(),
+  };
+
+  notes = notes.concat(newnote);
+  console.log(notes);
+  res.json(newnote);
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoin" });
+};
+
+app.use(unknownEndpoint);
+
+const port = 3001;
+app.listen(port, () => console.log("Server is running"));
